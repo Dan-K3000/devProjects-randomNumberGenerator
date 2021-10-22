e22616da75df711beace6eeaa2762eaf22842a52
Dan-K3000 <69226654+Dan-K3000@users.noreply.github.com>
2021-10-21 23:46:18 -0600

    Add files via upload

---
+++ randomNumberGenerator.js
@@ -0,0 +1,15 @@
+function getAmount(){
+    var amount = document.getElementById("diceAmount").value;
+    var sides = document.getElementById("diceSides").value;
+    let i = 0;
+    let total = 0;
+    let rolls = "";
+
+    while (i < amount) {
+    var roll = Math.floor(Math.random() * sides +1);
+    total+=roll;
+    rolls+= roll+" | "
+    document.getElementById("result").innerHTML = "You rolled "+amount+" D"+sides+".<br> This was your result.<br><br> | "+rolls+"<br><br><u>TOTAL</u><br>"+total;
+    i++}
+    document.getElementById("result").className = "result";
+}
